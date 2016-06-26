c7b14e42ef77fd137fe6c686a1ea54df0b08bd74
jorenver <jorenver@espol.edu.ec>
2016-06-25 23:32:56 -0500

    mejora server

--- Server/server.c
+++ Server/server.c
@@ -2,51 +2,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define workNumbers 10
-
+#define workNumbers 1
+#define MAX 5
 //compilar gcc -o server server.c
 //ejecutar: ./server < entradas.txt > log.txt
 
 //arrego de enteros
-int sockets[workNumbers];
-int fill = 0; //posicion donde se ingresan los datos
-int use = 0; //posicion donde se leen los datos
-int count = 0; //numero de datos
-
+int conectionTaking = 0; //numero de conexiones atendidas
+int newSocket=0;
+int newSockets[MAX];
+int actual=0;
+int first=0;
+int count=0;
+int ban=1;
 
 //variables de condicion
-pthread_cond_t fill_cv = PTHREAD_COND_INITIALIZER;
+pthread_cond_t newConection_cv = PTHREAD_COND_INITIALIZER;
 //mutex
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 //pthread_cond_wait 
 //pthread_cond_signal
 
-void put(int value) {
-	sockets[fill] = value;
-	fill = (fill + 1) % workNumbers;
+void putNewSocket(int s){
+	newSockets[actual]=s;
+	actual=(actual+1)%MAX;
 	count++;
 }
 
-int get() {
-	int tmp = sockets[use];
-	use = (use + 1) % workNumbers;
+int getNewSocket(){
+	int x=newSockets[first];
+	first=(first+1)%MAX;
 	count--;
-	return tmp;
+	return x;
 }
 
 void * worker(void* arg){
-
+	int socket;
 	while(1) {
 		pthread_mutex_lock(&mutex);
 		while (count == 0)
-			pthread_cond_wait(&fill_cv, &mutex); // se duerme el worker
-		
-		int tmp = get();
+			pthread_cond_wait(&newConection_cv, &mutex); // se duerme el worker
+		socket=getNewSocket(); //cuando se despierta guarda el socket que se acabo de conectar
+		conectionTaking++;
 		pthread_mutex_unlock(&mutex);
 
-		//hacer todas las operaciones
-		//en este caso imprimir
-		printf("Procese el sokect %d\n", tmp);
+		int contador=999999;
+		while(contador!=0)
+			contador--;
+		printf("Procese el sokect: %d\n", socket);
+		pthread_mutex_lock(&mutex);
+		conectionTaking--; // libero este worker
+		pthread_mutex_unlock(&mutex);
 	}
 
 	return NULL;
@@ -83,6 +89,9 @@ int main(){
 
 	//BOSS
 	int i;
+	for(i=0; i< MAX; i++){
+		newSockets[i]=0;
+	}
 	//create a thread pool
 	for(i=0; i< workNumbers; i++){
 		pthread_t p;
@@ -91,30 +100,30 @@ int main(){
 	//
 
 	while (1) {
+		if(ban){
+			//leer de stdin
+			int socket; 
+			
+			char * input = inputString(stdin, 10);
+			if(*input=='\0'){
+				ban=0;
+				continue;
+			}
+			else{
+				socket = atoi(input);
+				//printf("Servidor recibe %d\n", socket );
+			}
 
-		//leer de stdin
-		int socket; 
-		
-		char * input = inputString(stdin, 10);
-		if(*input=='\0'){
-			printf("Ya no hay mas entradas\n");
-			return 0;
-		}
-		else{
-			socket = atoi(input);
-			printf("Lei %d\n", socket );
-		}
-
-		pthread_mutex_lock(&mutex);
-		if (count == workNumbers){
-			printf("Ya no hay hilos, perdio el dato %d\n", socket);
+			pthread_mutex_lock(&mutex);
+			if (conectionTaking == workNumbers){ //todos los workes estan ocupados
+				printf("Ya no hay hilos, perdio el dato %d\n", socket);
+				pthread_mutex_unlock(&mutex);
+				continue;
+			}
+			putNewSocket(socket);
+			pthread_cond_signal(&newConection_cv);
 			pthread_mutex_unlock(&mutex);
-			continue;
 		}
-		
-		put(socket);
-		pthread_cond_signal(&fill_cv);
-		pthread_mutex_unlock(&mutex);
 
 
 	}
